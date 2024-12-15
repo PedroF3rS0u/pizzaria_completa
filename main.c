3af9048dc09079458e04b0f8a79346e4e01bd960
PedroF3rS0u <161458669+PedroF3rS0u@users.noreply.github.com>
2024-12-15 18:21:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -15,14 +15,52 @@ void menu_principal() {
         scanf("%d", &opcao);
 
         switch (opcao) {
-            case 1:
-                listar_pizzas();
-                adicionar_pizza();
+            case 1: {
+                int opcao_pizza;
+                do {
+                    printf("=== Gerenciar Pizzas ===\n");
+                    printf("1. Adicionar Pizza\n");
+                    printf("2. Listar Pizzas\n");
+                    printf("3. Editar Pizza\n");
+                    printf("4. Remover Pizza\n");
+                    printf("0. Voltar ao Menu Principal\n");
+                    printf("Escolha uma opcao: ");
+                    scanf("%d", &opcao_pizza);
+
+                    switch (opcao_pizza) {
+                        case 1: adicionar_pizza(); break;
+                        case 2: listar_pizzas(); break;
+                        case 3: editar_pizza(); break;
+                        case 4: remover_pizza(); break;
+                        case 0: break;
+                        default: printf("Opcao invalida!\n");
+                    }
+                } while (opcao_pizza != 0);
                 break;
-            case 2:
-                listar_ingredientes();
-                adicionar_ingrediente();
+            }
+            case 2: {
+                int opcao_ingrediente;
+                do {
+                    printf("=== Gerenciar Ingredientes ===\n");
+                    printf("1. Adicionar Ingrediente\n");
+                    printf("2. Listar Ingredientes\n");
+                    printf("3. Editar Ingrediente\n");
+                    printf("4. Remover Ingrediente\n");
+                    printf("0. Voltar ao Menu Principal\n");
+                    printf("Escolha uma opcao: ");
+                    scanf("%d", &opcao_ingrediente);
+
+                    switch (opcao_ingrediente) {
+                        case 1: adicionar_ingrediente(); break;
+                        case 2: listar_ingredientes(); break;
+                        case 3: editar_ingrediente(); break;
+                        case 4: remover_ingrediente(); break;
+                        case 0: break;
+                        default: printf("Opcao invalida!\n");
+                    }
+                } while (opcao_ingrediente != 0);
                 break;
+            }
             case 3:
                 exportar_pizzas();
                 exportar_ingredientes();
